10ea57e2b538d072d9969d969ef1b04c0f4cb454
Stanislav Shandyga <stanislavshandyga@gmail.com>
2020-03-20 14:44:04 +0200

    Update footer with Github link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,6 +3,7 @@ import * as React from "react"
 export default function Footer(): JSX.Element {
   return (
     <footer className="footer">
+      <a href="https://github.com/stsh89/cv">Github</a>
     </footer>
   )
 }
