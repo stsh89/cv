36fc99a2fcc34e2eb863a518e2df31dd7de9e352
Stanislav Shandyga <stanislavshandyga@gmail.com>
2020-03-20 23:13:16 +0200

    Add basic colors and update code

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,9 @@ import * as React from "react"
 export default function Footer(): JSX.Element {
   return (
     <footer className="footer">
-      <a href="https://github.com/stsh89/cv">Github</a>
+      <div className="footer-body">
+        <a href="https://github.com/stsh89/cv">Github</a>
+      </div>
     </footer>
   )
 }
--- src/components/personal_info.tsx
+++ src/components/personal_info.tsx
@@ -3,12 +3,11 @@ import Section from "./common/section"
 import PageTitle from "./page_title"
 
 export default function PersonalInfo(props: {
-  name: string, dateOfBirth: string, maritalStatus: string
+  dateOfBirth: string, maritalStatus: string
 }): JSX.Element {
   return (
     <Section>
       <div>
-        <PageTitle name={props.name} />
         <ul>
           <li>Date of birth: {props.dateOfBirth}</li>
           <li>Marital status: {props.maritalStatus}</li>
