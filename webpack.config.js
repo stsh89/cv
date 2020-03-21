6d3c01580cd9a8fc58fe8b8dda8a16c14d79793f
Stanislav Shandyga <stanislavshandyga@gmail.com>
2020-03-21 14:49:57 +0200

    Create new build

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
         test: /\.(png|jpe?g|gif)$/i,
         loader: 'file-loader',
         options: {
-          outputPath: 'images',
+          outputPath: '/dist/images',
         },
       }
     ]
