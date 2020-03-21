658f6d4f13dec82e7e123341a9ee050c500bd469
Stanislav Shandyga <stanislavshandyga@gmail.com>
2020-03-21 14:54:10 +0200

    Try to fix image path

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
         test: /\.(png|jpe?g|gif)$/i,
         loader: 'file-loader',
         options: {
-          outputPath: '/dist/images',
+          outputPath: '/dist/cv/images',
         },
       }
     ]
