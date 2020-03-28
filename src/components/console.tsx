95cbbf7c9b1a3ea0897c5bb0908f3906bf89d810
Stanislav Shandyga <stanislavshandyga@gmail.com>
2020-03-28 11:58:06 +0200

    Apply basic styling

--- src/components/console.tsx
+++ src/components/console.tsx
@@ -3,22 +3,22 @@ import * as userInfo from "..//../data/user_info.json"
 
 export default function Console(): JSX.Element {
   return (
-    <div className="page">
-      <div className="page-body">
-        <div className="page-body__overview">
+    <div className="console">
+      <div className="dashboard">
+        <div className="dashboard__overview">
           <section>
             <h2>Profile</h2>
             <p>{userInfo.summary}</p>
           </section>
         </div>
-        <div className="page-body__summary">
+        <div className="dashboard__summary">
           <h1>{userInfo.name}</h1>
           <h2>{userInfo.position}</h2>
           <section>
             <p>Contacts:</p>
             <ul className="list">
               <li>E-mail: stanislavshandyga@gmail.com</li>
-              <li>Telegram: <a href="https://t.me/iW7xYVKP6OMU5qwF">Stan</a></li>
+              <li>Telegram: <a className="link" href="https://t.me/iW7xYVKP6OMU5qwF">Stan</a></li>
             </ul>
           </section>
         </div>
