49812605f134d3c0501cd02e8b850dd9fbb1fd92
Stanislav Shandyga <stanislavshandyga@gmail.com>
2020-03-29 17:11:43 +0300

    Update link styles

--- src/components/homepage/footer.tsx
+++ src/components/homepage/footer.tsx
@@ -5,8 +5,7 @@ export default function Footer():JSX.Element {
     <div className="homepage-footer">
       <p className="homepage-footer__text">
         This is free and unencumbered software released into the public domain.
-        For more information, please refer to
-        <a className="homepage-link" href="https://unlicense.org">https://unlicense.org</a>
+        For more information, please refer to <a className="homepage-link" href="https://unlicense.org">https://unlicense.org</a>
       </p>
       <p className="homepage-footer__text">
         2020
