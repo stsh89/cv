18feaaaa7268023623b55f156305220222174133
Stanislav Shandyga <stanislavshandyga@gmail.com>
2020-04-01 22:38:17 +0300

    Update footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -7,12 +7,15 @@ export default function Footer():JSX.Element {
         This is free and unencumbered software released into the public domain.
         For more information, please refer to <a className="footer-link" href="https://unlicense.org">https://unlicense.org</a>
       </p>
-      <p className="footer__text">
-        2020
-        / CV
-        / <a className="footer-link" href="https://github.com/stsh89/cv">https://github.com/stsh89/cv/</a>
-        &nbsp;/ <a className="footer-link" href="https://stsh89.github.io/cv/">https://stsh89.github.io/cv/</a>
-      </p>
+      <ul className="footer-list">
+        <li className="footer-list__item">CV 2020</li>
+        <li className="footer-list__item">
+          Source: <a className="footer-link" href="https://github.com/stsh89/cv">https://github.com/stsh89/cv/</a>
+        </li>
+        <li className="footer-list__item">
+          Live version: <a className="footer-link" href="https://stsh89.github.io/cv/">https://stsh89.github.io/cv/</a>
+        </li>
+      </ul>
     </div>
   )
 }
