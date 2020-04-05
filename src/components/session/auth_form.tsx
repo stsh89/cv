0a1bab18338f63613fc11e53f9ab370ee9f3bbd6
Stanislav Shandyga <stanislavshandyga@gmail.com>
2020-04-05 11:37:10 +0300

    Add fresh build

--- src/components/session/auth_form.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react"
-
-export default function AuthForm(): JSX.Element {
-  return (
-    <form action="/" method="GET">
-      <input type="password" name="api-key" />
-    </form>
-  )
-}
