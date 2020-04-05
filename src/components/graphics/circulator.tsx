d29bb98b0b72b05a540ca72997a5d45ffe3b6265
Stanislav Shandyga <stanislavshandyga@gmail.com>
2020-04-05 11:40:18 +0300

    Fix eslint

--- src/components/graphics/circulator.tsx
+++ src/components/graphics/circulator.tsx
@@ -8,13 +8,13 @@ export default function Circulator(props: {
   return(
     <svg width="120" height="120">
       <circle cx="50%" cy="50%" r="50" fill="none"
-        stroke={props.borderColor} stroke-dasharray="32" stroke-width="2">
+        stroke={props.borderColor} strokeDasharray="32" strokeWidth="2">
         <animateTransform attributeName="transform"
           attributeType="XML" type="rotate" from="0 60 60"
           to="360 60 60" dur="3s" begin="0" repeatCount="indefinite"/>
       </circle>
-      <text x="60" y="60" dominant-baseline="middle" text-anchor="middle"
-        fill={props.textColor} font-weight="bold">{props.text}</text>
+      <text x="60" y="60" dominantBaseline="middle" textAnchor="middle"
+        fill={props.textColor} fontWeight="bold">{props.text}</text>
     </svg>
   )
 }
