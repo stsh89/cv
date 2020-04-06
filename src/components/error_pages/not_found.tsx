dfd5cec993c1fa1a1750cb648dce5bbb2115df0d
Stanislav Shandyga <stanislavshandyga@gmail.com>
2020-04-06 16:15:23 +0300

    Add link to to home page from not found

--- src/components/error_pages/not_found.tsx
+++ src/components/error_pages/not_found.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import Circulator from "../graphics/circulator"
+import {Link} from "react-router-dom"
 
 export default function NotFound(): JSX.Element {
   const [textValue, setTextValue] = React.useState(0)
@@ -15,9 +16,17 @@ export default function NotFound(): JSX.Element {
 
   return (
     <div className="error-page">
-      <main className="error-page-not-found">
+      <main className="not-found-page">
         <Circulator borderColor="#5afd5e" textColor="#d5d5d5" text={textValue.toString()} />
-        <h1 className="error-page-not-found__title hidden">Not Found</h1>
+        <h1 className="not-found-page__title">Page Not Found</h1>
+        <div className="not-found-page__body">
+          <p className="not-found-page-text">
+            The HTTP 404, 404 Not Found, 404, Page Not Found, or Server Not Found error message is a Hypertext Transfer Protocol (HTTP) standard response code, in computer network communications, to indicate that the browser was able to communicate with a given server, but the server could not find what was requested. The error may also be used when a server does not wish to disclose whether it has the requested information or not.
+          </p>
+          <p className="not-found-page-text">
+            Go to <Link to="/" className="not-found-page-link">Homepage</Link>
+          </p>
+        </div>
       </main>
     </div>
   )
