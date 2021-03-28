542ac46d0bde95141ffd22571b05260b792dc8bf
Stanislav Shandyga <ssh@anadeainc.com>
2021-03-28 10:57:19 +0300

    Closes issue #4

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,10 +1,12 @@
 import * as React from "react"
 
 export default function Footer(): JSX.Element {
+  const year = new Date().getUTCFullYear()
+
   return (
     <footer className="footer">
       <ul className="footer-list">
-        <li className="footer-list__item">CV / 2020</li>
+        <li className="footer-list__item">CV / {year}</li>
         <li className="footer-list__item">
           Source: <a className="footer-link" href="https://github.com/stsh89/cv">https://github.com/stsh89/cv/</a>
         </li>
